eee3fe1ed7f51ab02af0585358bee8bc89583fd4
Christoffer Andersson <chan8@kth.se>
2022-06-16 04:35:13 +0200

    moved template into visitor and improved error handling in main file

--- generator/cmd/main.go
+++ generator/cmd/main.go
@@ -1,31 +1,48 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"go/parser"
 	"go/token"
 	"log"
 	"os"
 	"strings"
 
-	"github.com/stoffand/go/validator/generator"
+	"github.com/stoffand/go-validator/generator"
 )
 
+// how to handle errors (with multiple files)
 func main() {
+	// Input files
+	flag.Parse()
+	fileNames := flag.Args()
+	if len(fileNames) == 0 {
+		log.Fatal("no files specified")
+	}
+
 	// Initialize
 	fs := token.NewFileSet()
 
 	// Get file
-	for _, fName := range os.Args[1:] {
+	for _, fName := range fileNames {
 		f, err := parser.ParseFile(fs, fName, nil, parser.ParseComments)
 		if err != nil {
-			log.Fatalf("could not parse %s: %v", fName, err)
+			fmt.Fprintf(os.Stderr, "%s: could not parse file\n", fName)
+			continue
 		}
 
 		// Traverse fileData and create template
 		fileData := generator.GetFileData(f)
-		templateData, err := fileData.CreateTemplate(fName)
+		if len(fileData.Types) == 0 {
+			fmt.Fprintf(os.Stderr, "%s: did not contain any parseable types\n", fName)
+			continue
+		}
+
+		templateData, err := fileData.CreateTemplate()
 		if err != nil {
-			panic(err)
+			fmt.Fprintf(os.Stderr, "%s: could not create template\n", fName)
+			continue
 		}
 
 		// Get filename (without path)
@@ -35,9 +52,8 @@ func main() {
 		// Write to file
 		err = os.WriteFile("vgen_"+fileName, templateData, 0644)
 		if err != nil {
-			panic(err)
+			fmt.Fprintf(os.Stderr, "%s: could write to files\n", fName)
+			continue
 		}
-
 	}
-
 }
--- generator/template.go
+++ generator/template.go
@@ -6,11 +6,11 @@ import (
 	"text/template"
 )
 
-func (d Data) CreateTemplate(filename string) ([]byte, error) {
+func (d Data) CreateTemplate() ([]byte, error) {
 	buf := new(bytes.Buffer)
 
 	// Template
-	tmpl := template.Must(template.New("template.tmpl").ParseFiles("template.tmpl"))
+	tmpl := template.Must(template.New("template.tmpl").Parse(tmplStr))
 	err := tmpl.Execute(buf, d)
 	if err != nil {
 		panic(err)
@@ -24,3 +24,68 @@ func (d Data) CreateTemplate(filename string) ([]byte, error) {
 
 	return formatted, nil
 }
+
+const (
+	tmplStr = `
+	// Generated file DO NOT EDIT
+
+	package {{ .Pkg }}
+	
+	import (
+		"github.com/stoffand/go-validator/validation"
+	)
+	
+	{{ range .Types }} 
+	
+	// Input struct
+	type {{ .Name }}In struct {
+		{{- range .Fields }}
+			{{ .Name }} {{ .In }}
+		{{- end }}
+	}
+	
+	// Convert input struct to original
+	func (in {{ .Name }}In) Convert() {{ .Name }} {
+		res := {{ .Name }}{}
+		{{- range .Fields }}
+			if in.{{ .Name }} != nil {
+				{{ .Convert }}
+			}
+		{{- end }}
+		return res
+	}
+	// Rules
+	type {{ .Name }}Rules struct {
+		{{- range .Fields }}
+			{{ .Name }} {{ .Rule }}            
+		{{- end }}
+	}
+	
+	// Validate required and validation rules
+	func (r {{ .Name }}Rules) Validate(in {{ .Name }}In) error {
+		var errs validation.StructErr
+		{{- range .Fields }}
+			if in.{{ .Name }} != nil {
+				if err := r.{{ .Name }}.Validate(*in.{{ .Name }}); err != nil {
+					errs.AddError("{{ .Name }}", err)
+				}
+			}  {{ if not .Pointer }} else {
+				errs.AddError("{{ .Name }}", validation.RequiredErr{})
+			} {{ end }}
+		{{- end }} 
+		if len(errs.FailedFields) == 0 {
+			return nil
+		}
+		return errs
+	}
+	
+	func (r {{ .Name }}Rules) ValidatedConvert(in {{ .Name }}In) ({{ .Name }}, error) {
+		err := r.Validate(in)
+		if err != nil {
+			return {{ .Name }}{}, err
+		}
+		return in.Convert(), nil
+	}
+	
+	{{ end }} `
+)
