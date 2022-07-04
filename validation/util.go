bf441c036baefb431cdfa1ec5f831b47e2c501fb
Christoffer Andersson <chan8@kth.se>
2022-07-04 03:49:08 +0200

    removed old function

--- validation/util.go
+++ validation/util.go
@@ -10,18 +10,6 @@ func P[T any](in T) *T {
 	return &in
 }
 
-func MapToError(in map[string]string) error {
-	if in == nil {
-		return nil
-	}
-	fmt.Println(in)
-	j, err := json.Marshal(in)
-	if err != nil {
-		log.Fatal(err)
-	}
-	return fmt.Errorf(string(j))
-}
-
 func Debug(where string, a any) {
 	j, err := json.MarshalIndent(a, "", "    ")
 	if err != nil {
